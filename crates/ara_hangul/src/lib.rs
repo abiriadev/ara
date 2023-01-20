0f4f98712e298af54f2276c0b2ae7dc4397a94fe
Abiria <abiria.dev@gmail.com>
2023-01-21 08:13:54 +0900

    feat: add conversion from subtype to supertype

--- crates/ara_hangul/src/lib.rs
+++ crates/ara_hangul/src/lib.rs
@@ -2,7 +2,27 @@
 
 pub struct Hangul(char);
 
+impl From<Jamo> for Hangul {
+	fn from(value: Jamo) -> Self { todo!() }
+}
+
+impl From<CompleteHangul> for Hangul {
+	fn from(value: CompleteHangul) -> Self { todo!() }
+}
+
 pub struct Jamo(char);
+
+impl From<Choseong> for Jamo {
+	fn from(value: Choseong) -> Self { todo!() }
+}
+
+impl From<Jungseong> for Jamo {
+	fn from(value: Jungseong) -> Self { todo!() }
+}
+
+impl From<Jongseong> for Jamo {
+	fn from(value: Jongseong) -> Self { todo!() }
+}
 
 pub struct Choseong(char);
 
