bede491d605b914e750be43c5588d02a62bc628e
Abiria <abiria.dev@gmail.com>
2023-01-21 08:04:48 +0900

    feat: add basic types without implementation

--- crates/ara_hangul/src/lib.rs
+++ crates/ara_hangul/src/lib.rs
@@ -1,1 +1,19 @@
+#![allow(unused)]
 
+pub struct Hangul(char);
+
+pub struct Jamo(char);
+
+pub struct Choseong(char);
+
+pub struct Jungseong(char);
+
+pub struct Jongseong(char);
+
+pub struct CompleteHangul(char);
+
+pub struct DisassembledHangul {
+	choseong: Choseong,
+	junseong: Jungseong,
+	jongseong: Option<Jongseong>,
+}
