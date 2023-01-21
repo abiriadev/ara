19a7d1c89c7394a05bc1ca7c227168ca6bfa93f7
Abiria <abiria.dev@gmail.com>
2023-01-21 18:36:07 +0900

    test: add tests for `is_jamo` function

--- crates/ara_hangul/tests/test.rs
+++ crates/ara_hangul/tests/test.rs
@@ -1,42 +1,75 @@
 use ara_hangul::*;
 
-mod is_hangul {
+mod check_functions {
 	use super::*;
 
-	#[test]
-	fn must_ignore_ascii_characters() {
-		assert!(!Hangul::is_hangul(
-			char::from_u32(0).unwrap()
-		));
-		assert!(!Hangul::is_hangul(
-			char::from_u32(1).unwrap()
-		));
-		assert!(!Hangul::is_hangul('A'));
+	mod is_hangul {
+		use super::*;
+
+		#[test]
+		fn must_ignore_ascii_characters() {
+			assert!(!Hangul::is_hangul(
+				char::from_u32(0).unwrap()
+			));
+			assert!(!Hangul::is_hangul(
+				char::from_u32(1).unwrap()
+			));
+			assert!(!Hangul::is_hangul('A'));
+		}
+
+		#[test]
+		fn must_include_complete_hangul() {
+			assert!(Hangul::is_hangul('가'));
+			assert!(Hangul::is_hangul('각'));
+			assert!(Hangul::is_hangul('각'));
+			assert!(Hangul::is_hangul('힣'));
+		}
+
+		#[test]
+		fn must_include_partial_characters() {
+			assert!(Hangul::is_hangul('ㄱ'));
+			assert!(Hangul::is_hangul('ㅏ'));
+			assert!(Hangul::is_hangul('ㄿ'));
+		}
+
+		#[test]
+		fn must_ignore_old_hangul_characters() {
+			assert!(!Hangul::is_hangul('ㅹ'));
+			assert!(!Hangul::is_hangul('ᄭ'));
+		}
+
+		#[test]
+		fn must_ignore_hcf() {
+			assert!(!Hangul::is_hangul('\u{115F}'));
+		}
 	}
 
-	#[test]
-	fn must_include_complete_hangul() {
-		assert!(Hangul::is_hangul('가'));
-		assert!(Hangul::is_hangul('각'));
-		assert!(Hangul::is_hangul('각'));
-		assert!(Hangul::is_hangul('힣'));
-	}
+	mod is_jamo {
+		use super::*;
 
-	#[test]
-	fn must_include_partial_characters() {
-		assert!(Hangul::is_hangul('ㄱ'));
-		assert!(Hangul::is_hangul('ㅏ'));
-		assert!(Hangul::is_hangul('ㄿ'));
-	}
+		#[test]
+		fn must_include_consonants() {
+			assert!(Jamo::is_jamo('ㅍ'));
+		}
 
-	#[test]
-	fn must_ignore_old_hangul_characters() {
-		assert!(!Hangul::is_hangul('ㅹ'));
-		assert!(!Hangul::is_hangul('ᄭ'));
-	}
+		#[test]
+		fn must_include_double_consonants() {
+			assert!(Jamo::is_jamo('ㅉ'));
+		}
 
-	#[test]
-	fn must_ignore_hcf() {
-		assert!(!Hangul::is_hangul('\u{115F}'));
+		#[test]
+		fn must_include_double_final_consonants() {
+			assert!(Jamo::is_jamo('ㅀ'))
+		}
+
+		#[test]
+		fn must_include_vowels() {
+			assert!(Jamo::is_jamo('ㅗ'));
+		}
+
+		#[test]
+		fn must_include_diphthong() {
+			assert!(Jamo::is_jamo('ㅞ'));
+		}
 	}
 }
